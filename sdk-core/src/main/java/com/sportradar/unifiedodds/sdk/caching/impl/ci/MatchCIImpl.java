d49075d69e41bbb5502e6df9a798924a58db700d
David Hrovat <d.hrovat@sportradar.com>
2019-09-17 12:40:27 +0200

    Fix: possible multi-thread issues caching competitors (FEEDSDK-1259)

--- sdk-core/src/main/java/com/sportradar/unifiedodds/sdk/caching/impl/ci/MatchCIImpl.java
+++ sdk-core/src/main/java/com/sportradar/unifiedodds/sdk/caching/impl/ci/MatchCIImpl.java
@@ -21,6 +21,7 @@ import com.sportradar.unifiedodds.sdk.caching.exportable.ExportableCI;
 import com.sportradar.unifiedodds.sdk.caching.exportable.ExportableCacheItem;
 import com.sportradar.unifiedodds.sdk.caching.exportable.ExportableMatchCI;
 import com.sportradar.unifiedodds.sdk.entities.BookingStatus;
+import com.sportradar.unifiedodds.sdk.entities.Competitor;
 import com.sportradar.unifiedodds.sdk.entities.EventStatus;
 import com.sportradar.unifiedodds.sdk.entities.Fixture;
 import com.sportradar.unifiedodds.sdk.exceptions.ObjectNotFoundException;
@@ -172,6 +173,11 @@ class MatchCIImpl implements MatchCI, ExportableCacheItem {
      * A {@link ReentrantLock} used to synchronize event timeline request operations
      */
     private final ReentrantLock timelineRequest = new ReentrantLock();
+
+    /**
+     * A {@link ReentrantLock} used to synchronize event competitors request operations
+     */
+    private final ReentrantLock competitorRequest = new ReentrantLock();
 
     /**
      * An indication on how should be the SDK exceptions handled
@@ -1074,29 +1080,38 @@ class MatchCIImpl implements MatchCI, ExportableCacheItem {
     private void cacheCompetitors(List<SAPITeamCompetitor> competitors, Locale locale) {
         Preconditions.checkNotNull(locale);
 
-        if (competitors == null) {
+        if (competitors == null || competitors.isEmpty()) {
             return;
         }
 
-        competitorIds = new ArrayList<>(competitors.size());
-        competitorQualifiers = new HashMap<>(competitors.size());
-        competitorDivisions = new HashMap<>(competitors.size());
+        competitorRequest.lock();
+        try {
+            competitorIds = new ArrayList<>(competitors.size());
+            competitorQualifiers = new HashMap<>(competitors.size());
+            competitorDivisions = new HashMap<>(competitors.size());
 
-        competitors.forEach(inputC -> {
-            URN parsedId = URN.parse(inputC.getId());
+            competitors.forEach(inputC -> {
+                URN parsedId = URN.parse(inputC.getId());
                 competitorIds.add(parsedId);
 
-            if (inputC.getQualifier() != null) {
-                competitorQualifiers.put(parsedId, inputC.getQualifier());
-            }
-            if (inputC.getDivision() != null) {
-                competitorDivisions.put(parsedId, inputC.getDivision());
-            }
-        });
+                if (inputC.getQualifier() != null) {
+                    competitorQualifiers.put(parsedId, inputC.getQualifier());
+                }
+                if (inputC.getDivision() != null) {
+                    competitorDivisions.put(parsedId, inputC.getDivision());
+                }
+            });
 
-        competitorsReferences = SdkHelper.parseTeamCompetitorsReferences(competitors, competitorsReferences);
+            competitorsReferences = SdkHelper.parseTeamCompetitorsReferences(competitors, competitorsReferences);
 
-        loadedCompetitorLocales.add(locale);
+            loadedCompetitorLocales.add(locale);
+        } catch (Exception e) {
+            String[] cs1 = competitors.stream().map(SAPITeamCompetitor::getId).toArray(String[]::new);
+            String cs = String.join(",", cs1);
+            handleException(String.format("cacheCompetitors(%s, %s)", cs, locale), e);
+        } finally {
+            competitorRequest.unlock();
+        }
     }
 
     /**
