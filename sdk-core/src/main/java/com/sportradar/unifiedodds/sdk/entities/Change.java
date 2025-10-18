ab0a66e270e46f6ad2fca762c3f3ac9dacbec50e
gbq6 <24852447+gbq6@users.noreply.github.com.>
2025-10-18 21:54:26 +0200

    Extracted comparator

---
+++ sdk-core/src/main/java/com/sportradar/unifiedodds/sdk/entities/Change.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) Sportradar AG. See LICENSE for full license governing this code
+ */
+
+package com.sportradar.unifiedodds.sdk.entities;
+
+import com.sportradar.utils.Urn;
+import java.util.Date;
+
+/**
+ * Defines methods used to access data of a change
+ */
+public interface Change {
+    /**
+     * Returns the {@link Urn} instance specifying the sport event
+     *
+     * @return - the {@link Urn} instance specifying the sport event
+     */
+    Urn getSportEventId();
+
+    /**
+     * Returns the {@link Date} instance specifying the last update time
+     *
+     * @return - the {@link Date} instance specifying the last update time
+     */
+    Date getUpdateTime();
+}
--- sdk-core/src/main/java/com/sportradar/unifiedodds/sdk/entities/FixtureChange.java
+++ sdk-core/src/main/java/com/sportradar/unifiedodds/sdk/entities/FixtureChange.java
@@ -4,24 +4,7 @@
 
 package com.sportradar.unifiedodds.sdk.entities;
 
-import com.sportradar.utils.Urn;
-import java.util.Date;
-
 /**
  * Defines methods used to access data of a fixture change
  */
-public interface FixtureChange {
-    /**
-     * Returns the {@link Urn} instance specifying the sport event
-     *
-     * @return - the {@link Urn} instance specifying the sport event
-     */
-    Urn getSportEventId();
-
-    /**
-     * Returns the {@link Date} instance specifying the last update time
-     *
-     * @return - the {@link Date} instance specifying the last update time
-     */
-    Date getUpdateTime();
-}
+public interface FixtureChange extends Change {}
